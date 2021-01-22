b4ea047f5ccff4baeecf3e5ca7fc5113c188b42f
tom_m <t.markey@mailfence.com>
2021-01-22 13:55:08 -0400

    testing timer python

---
+++ muffintimer/muffintimer.py
@@ -0,0 +1,27 @@
+import time
+import simpleaudio as sa
+
+
+filename = 'chime_up.wav'
+wave_obj = sa.WaveObject.from_wave_file(filename)
+
+
+print('Imput timer duration in Minutes:')
+
+
+
+max_time =float(input())*60
+print('Max time:', max_time)
+start = time.time() 
+elapsed =0;
+
+while elapsed < max_time:
+	now =time.time()
+	elapsed = now  - start
+	print(elapsed)
+	if elapsed > max_time:
+		print('Muffins Done!')
+		play_obj = wave_obj.play()
+		play_obj.wait_done()  # Wait until sound has finished playing
+		break
+			
